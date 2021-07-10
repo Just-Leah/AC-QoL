0026a27abc41b2358b05d9199af2aa4ec38c77f5
Stonewall0210 <76092465+Stonewall0210@users.noreply.github.com>
2021-07-10 11:26:07 +1000

    Separated Buttons into

--- src/main/java/io/github/stonewall0210/gui/warpGui.java
+++ src/main/java/io/github/stonewall0210/gui/warpGui.java
@@ -3,17 +3,41 @@ package io.github.stonewall0210.gui;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.*;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import io.github.cottonmc.cotton.gui.widget.icon.ItemIcon;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.client.MinecraftClient;
 
 public class warpGui extends LightweightGuiDescription {
     public warpGui() {
-        WPlainPanel root = new WPlainPanel();
-        setRootPanel(root);
-        root.setSize(200, 200);
-        root.setInsets(Insets.ROOT_PANEL);
+        WPlainPanel main = new WPlainPanel();
+        main.setSize(200, 60);
+        main.setInsets(Insets.ROOT_PANEL);
+        WPlainPanel QoL = new WPlainPanel();
+        QoL.setSize(200, 80);
+        QoL.setInsets(Insets.ROOT_PANEL);
+        WPlainPanel farms = new WPlainPanel();
+        farms.setSize(200, 40);
+        farms.setInsets(Insets.ROOT_PANEL);
+        WPlainPanel events = new WPlainPanel();
+        events.setSize(200, 40);
+        events.setInsets(Insets.ROOT_PANEL);
+        WPlainPanel archive = new WPlainPanel();
+        archive.setSize(200, 120);
+        archive.setInsets(Insets.ROOT_PANEL);
+
+        WTabPanel warpTabs = new WTabPanel();
+        warpTabs.add(main, tab -> tab.icon(new ItemIcon(new ItemStack(Items.ARMOR_STAND))).tooltip(new TranslatableText("gui.ac_qol.warp.tabs.main")));
+        warpTabs.add(QoL, tab -> tab.icon(new ItemIcon(new ItemStack(Items.COOKIE))).tooltip(new TranslatableText("gui.ac_qol.warp.tabs.qol")));
+        warpTabs.add(farms, tab -> tab.icon(new ItemIcon(new ItemStack(Items.FARMLAND))).tooltip(new TranslatableText("gui.ac_qol.warp.tabs.farms")));
+        warpTabs.add(events, tab -> tab.icon(new ItemIcon(new ItemStack(Items.DRAGON_EGG))).tooltip(new TranslatableText("gui.ac_qol.warp.tabs.events")));
+        warpTabs.add(archive, tab -> tab.icon(new ItemIcon(new ItemStack(Items.BUNDLE))).tooltip(new TranslatableText("gui.ac_qol.warp.tabs.archive")));
+        setRootPanel(warpTabs);
 
         WButton calmrooms = new WButton(new TranslatableText("button.ac_qol.warp.calmrooms"));
         calmrooms.setOnClick(() -> {
@@ -21,155 +45,169 @@ public class warpGui extends LightweightGuiDescription {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp calmrooms");
             }
         });
-        root.add(calmrooms, 0, 0, 100, 20);
+        QoL.add(calmrooms, 0, 0, 200, 20);
         WButton canada = new WButton(new TranslatableText("button.ac_qol.warp.canada"));
         canada.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp canada");
             }
         });
-        root.add(canada, 0, 20, 100, 20);
+        events.add(canada, 0, 0, 200, 20);
         WButton endermanfarm = new WButton(new TranslatableText("button.ac_qol.warp.endermanfarm"));
         endermanfarm.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp endermanfarm");
             }
         });
-        root.add(endermanfarm, 0, 40, 100, 20);
+        farms.add(endermanfarm, 0, 0, 100, 20);
         WButton farm = new WButton(new TranslatableText("button.ac_qol.warp.farm"));
         farm.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp farm");
             }
         });
-        root.add(farm, 0, 60, 100, 20);
+        farms.add(farm, 0, 20, 100, 20);
         WButton gwr = new WButton(new TranslatableText("button.ac_qol.warp.gwr"));
         gwr.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp gwr");
             }
         });
-        root.add(gwr, 0, 80, 100, 20);
+        events.add(gwr, 0, 20, 200, 20);
         WButton jobs = new WButton(new TranslatableText("button.ac_qol.warp.jobs"));
         jobs.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp jobs");
             }
         });
-        root.add(jobs, 0, 100, 100, 20);
+        QoL.add(jobs, 0, 20, 200, 20);
         WButton minigames = new WButton(new TranslatableText("button.ac_qol.warp.minigames"));
         minigames.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp minigames");
             }
         });
-        root.add(minigames, 0, 120, 100, 20);
+        main.add(minigames, 100, 20, 100, 20);
         WButton mobfarm = new WButton(new TranslatableText("button.ac_qol.warp.mobfarm"));
         mobfarm.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp mobfarm");
             }
         });
-        root.add(mobfarm, 0, 140, 100, 20);
+        farms.add(mobfarm, 100, 0, 100, 20);
         WButton shops = new WButton(new TranslatableText("button.ac_qol.warp.shops"));
         shops.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp shops");
             }
         });
-        root.add(shops, 0, 160, 100, 20);
+        main.add(shops, 0, 40, 100, 20);
         WButton spamrooms = new WButton(new TranslatableText("button.ac_qol.warp.spamrooms"));
         spamrooms.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp spamrooms");
             }
         });
-        root.add(spamrooms, 0, 180, 100, 20);
+        QoL.add(spamrooms, 0, 40, 200, 20);
         WButton spawn = new WButton(new TranslatableText("button.ac_qol.warp.spawn"));
         spawn.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
-                MinecraftClient.getInstance().player.sendChatMessage("/warp spawn");
+                MinecraftClient.getInstance().player.sendChatMessage("/spawn");
             }
         });
-        root.add(spawn, 0, 200, 100, 20);
+        main.add(spawn, 0, 0, 100, 20);
+        WButton spawnArchive = new WButton(new TranslatableText("button.ac_qol.warp.spawn"));
+        spawn.setOnClick(() -> {
+            if (MinecraftClient.getInstance().player != null) {
+                MinecraftClient.getInstance().player.sendChatMessage("/spawn");
+            }
+        });
+        archive.add(spawnArchive, 0, 0, 200, 20);
         WButton spawn1 = new WButton(new TranslatableText("button.ac_qol.warp.spawn1"));
         spawn1.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp spawn1");
             }
         });
-        root.add(spawn1, 100, 0, 100, 20);
+        archive.add(spawn1, 0, 20, 100, 20);
         WButton spawn2 = new WButton(new TranslatableText("button.ac_qol.warp.spawn2"));
         spawn2.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp spawn2");
             }
         });
-        root.add(spawn2, 100, 20, 100, 20);
+        archive.add(spawn2, 100, 20, 100, 20);
         WButton spawn3 = new WButton(new TranslatableText("button.ac_qol.warp.spawn3"));
         spawn3.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp spawn3");
             }
         });
-        root.add(spawn3, 100, 40, 100, 20);
+        archive.add(spawn3, 0, 40, 100, 20);
         WButton spawn4 = new WButton(new TranslatableText("button.ac_qol.warp.spawn4"));
         spawn4.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp spawn4");
             }
         });
-        root.add(spawn4, 100, 60, 100, 20);
+        archive.add(spawn4, 100, 40, 100, 20);
         WButton tao = new WButton(new TranslatableText("button.ac_qol.warp.tao"));
         tao.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp tao");
             }
         });
-        root.add(tao, 100, 80, 100, 20);
+        QoL.add(tao, 0, 60, 200, 20);
         WButton tardis = new WButton(new TranslatableText("button.ac_qol.warp.tardis"));
         tardis.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp tardis");
             }
         });
-        root.add(tardis, 100, 100, 100, 20);
+        main.add(tardis, 100, 40, 100, 20);
+        WButton tardisArchive = new WButton(new TranslatableText("button.ac_qol.warp.tardis"));
+        tardis.setOnClick(() -> {
+            if (MinecraftClient.getInstance().player != null) {
+                MinecraftClient.getInstance().player.sendChatMessage("/warp tardis");
+            }
+        });
+        archive.add(tardisArchive, 0, 60, 200, 20);
         WButton tardis1 = new WButton(new TranslatableText("button.ac_qol.warp.tardis1"));
         tardis1.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp tardis1");
             }
         });
-        root.add(tardis1, 100, 120, 100, 20);
+        archive.add(tardis1, 0, 80, 100, 20);
         WButton tardis2 = new WButton(new TranslatableText("button.ac_qol.warp.tardis2"));
         tardis2.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp tardis2");
             }
         });
-        root.add(tardis2, 100, 140, 100, 20);
+        archive.add(tardis2, 100, 80, 100, 20);
         WButton trades = new WButton(new TranslatableText("button.ac_qol.warp.trades"));
         trades.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp trades");
             }
         });
-        root.add(trades, 100, 160, 100, 20);
+        farms.add(trades, 100, 20, 100, 20);
         WButton wither = new WButton(new TranslatableText("button.ac_qol.warp.wither"));
         wither.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp wither");
             }
         });
-        root.add(wither, 100, 180, 100, 20);
+        main.add(wither, 100, 0, 100, 20);
         WButton worlds = new WButton(new TranslatableText("button.ac_qol.warp.worlds"));
         worlds.setOnClick(() -> {
             if (MinecraftClient.getInstance().player != null) {
                 MinecraftClient.getInstance().player.sendChatMessage("/warp worlds");
             }
         });
-        root.add(worlds, 100, 200, 100, 20);
+        main.add(worlds, 0, 20, 100, 20);
 
-        root.validate(this);
+        warpTabs.validate(this);
     }
 }
